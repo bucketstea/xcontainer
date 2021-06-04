058c59023ae0f554a7ac69fb1bb5b9b53c9fab42
bucketstea <c.g.jung.2202@gmail.com>
2021-06-05 02:22:30 +0900

    1st commit

---
+++ xcont.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+SCRIPT_DIR=`dirname $0`
+cd $SCRIPT_DIR
+
+#除外するコンテナ名name_list
+xcntlist=`cat .xcontignore`
+echo -e $xcntlist "\nare excluded\n"
+
+#rm対象になるコンテナを絞り込む関数
+function cntList() {
+    docker ps -a|awk '{print $NF}'|tail -n +2 > /tmp/xcontall.txt
+    sort /tmp/xcontall.txt|cat > /tmp/sortedxcontall.txt
+    sed '/^$/d' .xcontignore > /tmp/xcontignore.txt
+    sort /tmp/xcontignore.txt|cat > /tmp/sortedxcontignore.txt
+    comm -23 /tmp/sortedxcontall.txt /tmp/sortedxcontignore.txt
+}
+
+newcnt=`cntList`
+
+
+#rm ~/Documents/script/sortedxcont* ~/Documents/script/xcontall.txt
+
+docker stop `echo $newcnt`
+if [ $? -eq 0 ]; then
+	echo -e "are Stoped\n"
+fi
+
+docker rm `echo $newcnt`
+if [ $? -eq 0 ]; then
+	echo -e "are Removed\n"
+fi
+
+docker ps -a | sed -e "s/CONTAINER.ID/CONTAINER_ID/g" | awk '{$1="";print}'
+echo -e "are Current Container\n"
+
