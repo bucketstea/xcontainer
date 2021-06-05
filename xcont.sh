fc9b80a7f3b7a0ea663b6fd25adaee2c9916d299
bucketstea <c.g.jung.2202@gmail.com>
2021-06-05 14:42:01 +0900

    refactoring

--- xcont.sh
+++ xcont.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
+##################################################カレントディレクトリの初期化
 SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 
-#除外するコンテナ名name_list
+##################################################除外するコンテナ名nameをlistでecho
 xcntlist=`cat .xcontignore`
-echo -e $xcntlist "\nare excluded\n"
+echo -e $xcntlist "\nare Excluded\n"
 
-#rm対象になるコンテナを絞り込む関数
+##################################################rm対象になるコンテナを絞り込む関数
 function cntList() {
     docker ps -a|awk '{print $NF}'|tail -n +2 > /tmp/xcontall.txt
     sort /tmp/xcontall.txt|cat > /tmp/sortedxcontall.txt
@@ -15,8 +17,10 @@ function cntList() {
     comm -23 /tmp/sortedxcontall.txt /tmp/sortedxcontignore.txt
 }
 
+##################################################コンテナ絞り込み関数の戻り値を代入
 newcnt=`cntList`
 
+##################################################コンテナの停止・削除処理(停止・削除できた場合はその旨echo)
 docker stop `echo $newcnt`
 if [ $? -eq 0 ]; then
 	echo -e "are Stoped\n"
@@ -27,8 +31,10 @@ if [ $? -eq 0 ]; then
 	echo -e "are Removed\n"
 fi
 
+##################################################最新のコンテナ稼働状況を表示
 docker ps -a | sed -e "s/CONTAINER.ID/CONTAINER_ID/g" | awk '{$1="";print}'
 echo -e "are Current Container\n"
 
-cd /tmp/ && rm `ls /tmp/xcont/|grep xcont`; cd $SCRIPT_DIR
+##################################################一時的に生成したファイルを削除
+cd /tmp/ && rm `ls|grep xcont`; cd $SCRIPT_DIR
 
