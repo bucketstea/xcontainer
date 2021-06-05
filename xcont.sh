10af4c06f4197678ab120b41e8a324f376c074bd
bucketstea <c.g.jung.2202@gmail.com>
2021-06-06 03:59:39 +0900

    refactoring

--- xcont.sh
+++ xcont.sh
@@ -5,36 +5,47 @@ SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 
 ##################################################除外するコンテナ名nameをlistでecho
-xcntlist=`cat .xcontignore`
-echo -e $xcntlist "\nare Excluded\n"
+isFile=".xcontignore";
 
-##################################################rm対象になるコンテナを絞り込む関数
+if [ -e $isFile ]; then
+    xcntlist=`cat .xcontignore`;
+    cat .xcontignore | sed 's/ //g' | sed '/^$/d' > /tmp/ignorelist;
+    xcntlistnull=/tmp/ignorelist;
+    if [ -s $xcntlistnull ]; then
+        echo -e $xcntlist "\nare Excluded\n"
+        rm /tmp/ignorelist;
+    fi
+fi
+
+##################################################rm対象になるコンテナを絞り込む
 function cntList() {
-    docker ps -a|awk '{print $NF}'|tail -n +2 > /tmp/xcontall.txt
-    sort /tmp/xcontall.txt|cat > /tmp/sortedxcontall.txt
-    sed '/^$/d' .xcontignore > /tmp/xcontignore.txt
-    sort /tmp/xcontignore.txt|cat > /tmp/sortedxcontignore.txt
-    comm -23 /tmp/sortedxcontall.txt /tmp/sortedxcontignore.txt
+    mkdir /tmp/xcont.d 2> /dev/null;
+    docker ps -a|awk '{print $NF}'|tail -n +2 > /tmp/xcont.d/xcontall.txt; 
+    sort /tmp/xcont.d/xcontall.txt|cat > /tmp/xcont.d/sortedxcontall.txt;
+    sed '/^$/d' .xcontignore > /tmp/xcont.d/xcontignore.txt 2>&1;
+    sort /tmp/xcont.d/xcontignore.txt|cat > /tmp/xcont.d/sortedxcontignore.txt;
+    comm -23 /tmp/xcont.d/sortedxcontall.txt /tmp/xcont.d/sortedxcontignore.txt;
+    rm -rf /tmp/xcont.d 2> /dev/null;
 }
 
-##################################################コンテナ絞り込み関数の戻り値を代入
 newcnt=`cntList`
 
 ##################################################コンテナの停止・削除処理(停止・削除できた場合はその旨echo)
-docker stop `echo $newcnt`
+docker stop `echo $newcnt` 2> /dev/null
 if [ $? -eq 0 ]; then
-	echo -e "are Stoped\n"
+    echo -e "are Stoped\n"
+else
+    echo -e "There are no stoppable containers\n"
 fi
 
-docker rm `echo $newcnt`
+docker rm `echo $newcnt` 2> /dev/null
 if [ $? -eq 0 ]; then
 	echo -e "are Removed\n"
+else
+    echo -e "There are no deletable containers\n"
 fi
 
 ##################################################最新のコンテナ稼働状況を表示
 docker ps -a | sed -e "s/CONTAINER.ID/CONTAINER_ID/g" | awk '{$1="";print}'
 echo -e "are Current Container\n"
-
-##################################################一時的に生成したファイルを削除
-cd /tmp/ && rm `ls|grep xcont`; cd $SCRIPT_DIR
 
